f9b7b0bf95bfe08035ba13cd1ed40eb23dd6b42c
HarumaroJP <haruto09.game@gmail.com>
2023-07-29 17:19:45 +0900

    init project

---
+++ Assets/Editor/TextureCreator.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.AssetImporters;
+
+public class TextureCreator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() { }
+}
