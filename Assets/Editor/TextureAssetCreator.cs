1867bddfbe78de7c1f93c6ee11138bd93e6febce
HarumaroJP <haruto09.game@gmail.com>
2023-07-29 21:08:15 +0900

    単色テクスチャの生成

---
+++ Assets/Editor/TextureAssetCreator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+
+namespace Editor
+{
+    public class TextureAssetCreator
+    {
+        [MenuItem("Assets/Create/HaruTex")]
+        public static void CreateNewAsset()
+            => ProjectWindowUtil.CreateAssetWithContent("New Harutex.harutex", "");
+    }
+}
